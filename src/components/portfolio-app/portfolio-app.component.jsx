0f9562bdac475b9f5c67ed0cb3a4b02ca40aa1da
j-gens <jgens2@gmail.com>
2020-04-16 15:58:10 -0700

    Boilerplate functional component and styles sheet

---
+++ src/components/portfolio-app/portfolio-app.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './portfolio-app.styles.css';
+
+
+const PortfolioApp = ({ name, tagline, about, tech, url }) => (
+  <div className='portfolio-app'>
+    app app app
+  </div>
+);
+
+
+export default PortfolioApp;
