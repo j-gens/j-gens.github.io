b3714ebee1b853ea531c395bc38ef5979f138447
j-gens <jgens2@gmail.com>
2020-04-16 15:49:23 -0700

    Add information for About Me and Education sections

--- src/content/content.js
+++ src/content/content.js
@@ -1,30 +1,60 @@
-export const PORTFOLIO = [
-  {
-    name: 'reVIEW',
-    tagline: 'A review-aggregation app for popular media',
-    about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
-    tech: [
-      'JavaScript', 'React', 'styled-components', 'Webpack', 'Babel', 'Jest', 'Enzyme', 'SuperTest', 'Node/Express', 'MongoDB/Mongoose', 'AWS (EC2, S3)', 'Git/Github'
-    ],
-    url: {
-      github: '',
-      image: ''
+export const PORTFOLIO = {
+  apps: [
+    {
+      name: 'reVIEW',
+      tagline: 'A review-aggregation app for popular media',
+      about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
+      tech: [
+        'JavaScript', 'React', 'styled-components', 'Webpack', 'Babel', 'Jest', 'Enzyme', 'SuperTest', 'Node.js/Express', 'MongoDB/Mongoose', 'AWS (EC2, S3)', 'Git/Github'
+      ],
+      url: {
+        github: '',
+        image: ''
+      }
+    },
+    {
+      name: 'weWare',
+      tagline: 'A marketplace app for handmade goods and wares',
+      about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
+      tech: [
+        'JavaScript', 'React', 'Node.js/Express', 'SQL/PostgreSQL', 'New Relic', 'loader.io', 'Nginx', 'AWS (EC2)', 'Git/Github'
+      ],
+      url: {
+        github: '',
+        image: ''
+      }
     }
-  },
-  {
-    name: 'weWARE',
-    tagline: 'A marketplace app for handmade goods and wares',
-    about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
-    tech: [
-      'JavaScript', 'React', 'Node/Express', 'SQL/PostgreSQL', 'New Relic', 'loader.io', 'Nginx', 'AWS (EC2)', 'Git/Github'
-    ],
-    url: {
-      github: '',
-      image: ''
-    }
+  ]
+};
+
+
+export const ABOUTME = {
+  paragraphs: [
+    'My name is Julia, and I am a software engineer based in the San Francisco Bay Area.',
+    'What I appreciate most about coding is the near-limitless potential of creation - and the satisfaction that comes from seeing the work through to fruition.  I have always had a passion for solving problems and love to spend time writing clean, reusable code. ....Combining this innate curiosity...Combined with my experience in client service, I always approach new projects with the user experience in mind.  This people-first mentality...I take delight in solving problems, and....',
+    'When not working, I enjoy spending time drawing (recently added watercolor to the mix) and playing co-operative board games with my friends and family.  I am always on the lookout for tabletop game recommendations!'
+  ],
+  lincoln: {
+    about: 'Lincoln enjoys three square meals, harvesting the benefits from naps, and long walks to the park.',
+    image: 'images/linkadink-sm.png'
   }
-];
+};
 
 
-export const
-
+export default EDUSKILLS = {
+  education: [
+    {
+      school: 'University of Illinois at Urbana-Champaign',
+      detail: 'Bachelor of Science in Integrative Biology and Chemistry',
+      year: 2009
+    },
+    {
+      school: 'Hack Reactor at Galvanize',
+      detail: 'Software Engineer Immersive Program',
+      year: 2019
+    }
+  ],
+  skills: [
+    'JavaScript (ES6+)', 'React', 'Redux', 'HTML & (S)CSS', 'Webpack & Babel', 'Node.js & Express', 'SQL (MySQL, PostgreSQL)', 'NoSQL (MongoDB, Cassandra)'
+  ]
+};
