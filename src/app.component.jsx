79aeffb88e9680884bf35d37edcf57f2b0bf2e27
j-gens <jgens2@gmail.com>
2020-04-16 15:02:18 -0700

    Import child components

--- src/app.component.jsx
+++ src/app.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import Welcome from './components/welcome/welcome.component.jsx';
+import Portfolio from './components/portfolio/portfolio.component.jsx';
+import EduSkills from './components/edu-skills/edu-skills.component.jsx';
+import AboutMe from './components/about-me/about-me.component.jsx';
+import Contact from './components/contact/contact.component.jsx';
+
 import './app.styles.css';
 
 
