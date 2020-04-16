fea2d95d689b31ba659f43e0f8d73db553b32064
j-gens <jgens2@gmail.com>
2020-04-16 14:02:30 -0700

    Boilerplate configuration for webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: 'bundled.js',
+    path: './public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-react', '@babel/preset-env',
+              ],
+              plugins: [
+                '@babel/plugin-proposal-class-properties',
+              ],
+            },
+          },
+        ],
+      }, {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader', 'css-loader',
+        ],
+      },
+    ],
+  }
+};
