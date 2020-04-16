c1d5dca58762fa89f472b737046b61c47ec7b3dc
j-gens <jgens2@gmail.com>
2020-04-16 15:16:10 -0700

    Create file for content to be easily accessed and updated

---
+++ src/content/content.js
@@ -0,0 +1,30 @@
+export const PORTFOLIO = [
+  {
+    name: 'reVIEW',
+    tagline: 'A review-aggregation app for popular media',
+    about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
+    tech: [
+      'JavaScript', 'React', 'styled-components', 'Webpack', 'Babel', 'Jest', 'Enzyme', 'SuperTest', 'Node/Express', 'MongoDB/Mongoose', 'AWS (EC2, S3)', 'Git/Github'
+    ],
+    url: {
+      github: '',
+      image: ''
+    }
+  },
+  {
+    name: 'weWARE',
+    tagline: 'A marketplace app for handmade goods and wares',
+    about: 'My work on this application was done as part of a five-person remote team.  We adopted many Agile practices - including daily stand-ups and Trello boards for workflow visualization.  Every part of this application is a separate module.  My objective was to build the image carousel, with a focus on clean and functional design.  I constructed the module with React and styled-components for a dynamic and animated experience.  Testing was done with Jest, Enzyme, and SuperTest.',
+    tech: [
+      'JavaScript', 'React', 'Node/Express', 'SQL/PostgreSQL', 'New Relic', 'loader.io', 'Nginx', 'AWS (EC2)', 'Git/Github'
+    ],
+    url: {
+      github: '',
+      image: ''
+    }
+  }
+];
+
+
+export const
+
