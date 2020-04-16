7b13abf4bb4cc569dde449e19b992287b56e5a6e
j-gens <jgens2@gmail.com>
2020-04-16 14:32:07 -0700

    Boilerplate functional component and styles sheet

---
+++ src/components/portfolio/portfolio.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './portfolio.styles.css';
+
+
+const Portfolio = () => (
+  <div className='portfolio'>
+    portfolio
+  </div>
+);
+
+
+export default Portfolio;
