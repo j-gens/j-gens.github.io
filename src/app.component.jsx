93e8cc923c128eab84719d7820a1e756c642b627
j-gens <jgens2@gmail.com>
2020-04-20 08:34:55 -0700

    Add skills section and update order of child components

--- src/app.component.jsx
+++ src/app.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import Welcome from './components/welcome/welcome.component.jsx';
 import Portfolio from './components/portfolio/portfolio.component.jsx';
-import EduSkills from './components/edu-skills/edu-skills.component.jsx';
+import Education from './components/edu-skills/edu-skills.component.jsx';
+import Skills from './components/skills/skills.component.jsx';
 import AboutMe from './components/about-me/about-me.component.jsx';
 import Contact from './components/contact/contact.component.jsx';
 
@@ -11,7 +12,12 @@ import './app.styles.css';
 
 const App = () => (
   <div className='app'>
-    hello world
+    <Welcome />
+    <Portfolio />
+    <Skills />
+    <Education />
+    <AboutMe />
+    <Contact />
   </div>
 );
 
