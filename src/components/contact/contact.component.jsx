75e38b37f70c2a931c22ba3b72c76bbd33b0d001
j-gens <jgens2@gmail.com>
2020-04-20 07:54:48 -0700

    Add classNames for styling and links for professional sites

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -5,7 +5,29 @@ import './contact.styles.css';
 
 const Contact = () => (
   <div className='contact'>
-    contact
+    <span className='contact--title'>
+      Contact Me
+    </span>
+    <span className='contact--subtitle'>
+      I would love to hear from you!
+    </span>
+    <div className='contact--links'>
+      <div className='contact--link'>
+        <a href='mailto:juliatgens@gmail.com' target='_blank'>
+          Email
+        </a>
+      </div>
+      <div className='contact--link'>
+        <a href='https://www.linkedin.com/in/juliatgens/' target='_blank'>
+          LinkedIn
+        </a>
+      </div>
+      <div className='contact--link'>
+        <a href='https://github.com/j-gens' target='_blank'>
+          Github
+        </a>
+      </div>
+    </div>
   </div>
 );
 
