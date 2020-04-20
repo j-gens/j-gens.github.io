130c3476535f405f260f339ff6539c33d80d8391
j-gens <jgens2@gmail.com>
2020-04-20 08:30:05 -0700

    Boilerplate functional component and styles sheet

---
+++ src/components/skills/skills.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { EDUSKILLS } from '../../content/content.js';
+
+import './skills.styles.css';
+
+const Skills = () => (
+  <div className='skills'>
+
+  </div>
+);
+
+
+export default Skills;
