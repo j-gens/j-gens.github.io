0c46f2e548da7df9e6eaedd1c76c3715921f30a6
j-gens <jgens2@gmail.com>
2020-04-21 08:36:33 -0700

    Update classNames to match app themes

--- src/components/portfolio/portfolio.component.jsx
+++ src/components/portfolio/portfolio.component.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import PortfolioApp from '../portfolio-app/portfolio-app.component.jsx';
+import { PORTFOLIO } from '../../content/content.js';
+
 import './portfolio.styles.css';
 
 
 const Portfolio = () => (
   <div className='portfolio'>
-    portfolio
+    <span className='section-title'>
+      Portfolio
+    </span>
+    <div className='portfolio--content'>
+      {
+        PORTFOLIO.apps.map(app =>
+          <PortfolioApp key={app.name} app={app} />
+        )
+      }
+    </div>
   </div>
 );
 
