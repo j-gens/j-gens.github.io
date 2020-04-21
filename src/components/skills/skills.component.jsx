6a1c26eb3d26dfbdf9d8d360018197f1570294dd
j-gens <jgens2@gmail.com>
2020-04-21 08:50:22 -0700

    Break skills into smaller components

--- src/components/skills/skills.component.jsx
+++ src/components/skills/skills.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import { EDUSKILLS } from '../../content/content.js';
+import SkillSet from '../skill-set/skill-set.component.jsx';
 
 import './skills.styles.css';
 
 const Skills = () => (
   <div className='skills'>
-
+    <span className='section-title'>
+        Skills
+    </span>
+    <div className='skills--content'>
+      {
+        EDUSKILLS.skills.map(skill =>
+          <SkillSet key={skill.title} skill={skill} />
+        )
+      }
+    </div>
   </div>
 );
 
