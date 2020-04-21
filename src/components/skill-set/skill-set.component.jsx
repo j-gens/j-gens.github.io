eb31c7ce51638bff45aec079723166536a13e11b
j-gens <jgens2@gmail.com>
2020-04-21 08:50:01 -0700

    Boilerplate functional component and styles sheet

---
+++ src/components/skill-set/skill-set.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './skill-set.styles.css';
+
+
+const SkillSet = ({ title, tech }) => (
+  <div className='skill-set'>
+    hello
+  </div>
+);
+
+
+export default SkillSet;
