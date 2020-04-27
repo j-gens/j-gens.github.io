dfa84759d4634c6116d0123a9becad56dffd6709
j-gens <jgens2@gmail.com>
2020-04-27 16:51:17 -0700

    Add classNames for content placement and responsive styling

--- src/components/portfolio-app/portfolio-app.component.jsx
+++ src/components/portfolio-app/portfolio-app.component.jsx
@@ -3,11 +3,42 @@ import React from 'react';
 import './portfolio-app.styles.css';
 
 
-const PortfolioApp = ({ name, tagline, about, tech, url }) => (
-  <div className='portfolio-app'>
-    app app app
-  </div>
-);
+const PortfolioApp = (props) => {
+  const { name, tagline, about, tech, url } = props.app;
+  return (
+    <div className='portfolio-app subsection'>
+      <div className='portfolio-app--content'>
+        <span className='section-subtitle__large'>
+          {name}
+        </span>
+        <span className='section-subtitle__small'>
+          {tagline}
+        </span>
+        <div className='portfolio-app--about'>
+          {
+            about.map(item =>
+              <div className='porfolio-app--about--item' key={item}>
+                &#x2022; {item}
+              </div>
+            )
+          }
+        </div>
+        <div className='portfolio-app--tech'>
+          {
+            tech.map(string =>
+              <div className='portfolio-app--tech--stack' key={string}>
+                {string.toUpperCase()}
+              </div>
+              )
+          }
+        </div>
+      </div>
+      <div className='portfolio-app--image'>
+        <img src={url.image} alt={tagline} />
+      </div>
+    </div>
+  )
+}
 
 
 export default PortfolioApp;
