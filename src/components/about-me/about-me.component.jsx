2423566353b29165f43b33ff0c1be32821743e41
j-gens <jgens2@gmail.com>
2020-04-28 16:51:20 -0700

    Add classNames for text styling

--- src/components/about-me/about-me.component.jsx
+++ src/components/about-me/about-me.component.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { ABOUTME } from '../../content/content.js';
+
 import './about-me.styles.css';
 
 
 const AboutMe = () => (
   <div className='about-me'>
-    about me
+    <span className='section-title'>
+      About Me
+    </span>
+    <div className='about-me--content'>
+      {ABOUTME.paragraphs.map((paragraph, index) =>
+        <div className='about-me--content--p' key={index}>
+          {paragraph}
+        </div>
+      )}
+    </div>
   </div>
 );
 
