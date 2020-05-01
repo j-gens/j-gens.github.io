35998d44b83201b1fce1ee29dd74f6d5c6d58962
j-gens <jgens2@gmail.com>
2020-05-01 11:33:13 -0700

    Add content for each component - currently added 2 apps to portfolio

--- src/content/content.js
+++ src/content/content.js
@@ -33,42 +33,17 @@ export const PORTFOLIO = {
         github: 'https://github.com/j-gens/BTetsy-imagecarousel',
         image: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/weware-gen.jpg'
       }
-    },
-    // {
-    //   name: 'pug-commerce',
-    //   tagline: 'A review-aggregation app for popular media',
-    //   about: '',
-    //   tech: [
-    //     'JavaScript', 'React', 'Redux', 'Webpack', 'Babel', 'Node.js / Express', 'Firebase', 'AWS (EC2, S3)', 'Git / Github'
-    //   ],
-    //   url: {
-    //     github: 'https://github.com/j-gens/pug-commerce',
-    //     image: ''
-    //   }
-    // },
-    // {
-    //   name: 'Athletour',
-    //   tagline: 'Sports venues tracking app for hockey fans',
-    //   about: '',
-    //   tech: [
-    //     'JavaScript', 'React', 'Node.js / Express', 'MongoDB / Mongoose', 'HTML5 / CSS',  'AWS (EC2)', 'Git / Github'
-    //   ],
-    //   url: {
-    //     github: 'https://github.com/j-gens/MVP-Athletour',
-    //     image: ''
-    //   }
-    // }
+    }
   ]
 };
 
 
 export const ABOUTME = {
   paragraphs: [
-    'My name is Julia, and I am a software engineer based in the San Francisco Bay Area.',
-
-    'What I appreciate most about coding is the near-limitless potential of creation - and the satisfaction that comes from seeing the work through to fruition.  I have always had a passion for solving problems and love to spend time writing clean, reusable code. ....Combining this innate curiosity...Combined with my experience in client service, I always approach new projects with the user experience in mind.  This people-first mentality...I take delight in solving problems, and....',
-
-    'When not working, I enjoy spending time drawing (recently added watercolor to the mix) and playing co-operative board games with my friends and family.  I am always on the lookout for tabletop game recommendations!'
+    'Hello! I am a Fullstack Developer currently based in the San Francisco Bay Area.',
+    'I am driven by the goal of self-improvement, a significant piece of which is finding better and more efficient ways to solve a problem.  I utilize my logical mindset and high attention to detail for the purpose of debugging and informing how I write my own code.  My objective as a developer is to keep my code clean and readable to provide longevity and accessibility within a legacy codebase.',
+    'When not working, I enjoy spending time drawing (recently added watercolor to the mix), exploring the outdoors with my pug, and playing co-operative board games with my friends and family.  I am always on the lookout for tabletop game recommendations!',
+    'Check out below for more information on my portfolio and skills!'
   ],
   lincoln: {
     about: 'Lincoln enjoys three square meals, harvesting the benefits from naps, and long walks to the park.',
@@ -82,23 +57,54 @@ export const EDUSKILLS = {
     {
       school: 'University of Illinois at Urbana-Champaign',
       detail: 'Bachelor of Science in Integrative Biology and Chemistry',
-      year: 2009
+      year: 2009,
+      logo: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/UIUC-logo.png'
     },
     {
       school: 'Hack Reactor at Galvanize',
       detail: 'Software Engineer Immersive Program',
-      year: 2019
+      year: 2019,
+      logo: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/HR-logo.png'
     }
   ],
-  skills: {
-    frontend: [
-      'JavaScript (ES6+)', 'React', 'Redux', 'jQuery', 'HTML & (S)CSS', 'Webpack & Babel'
-    ],
-    backend: [
-      'Node.js & Express', 'MySQL', 'PostgreSQL', 'MongoDB', 'Cassandra'
-    ],
-    dev: [
-      'Git/Github', 'Mocha', 'Chai', 'Jest', 'Enzyme', 'AWS (EC2/S3)', 'Heroku'
-    ]
-  }
+  skills: [
+    {
+      title: 'Front-End',
+      tech: [
+        'JavaScript (ES6+)', 'React', 'Redux', 'jQuery', 'HTML & (S)CSS', 'Webpack & Babel'
+      ]
+    },
+    {
+      title: 'Back-End',
+      tech: [
+        'Node.js & Express', 'MySQL', 'PostgreSQL', 'MongoDB', 'Cassandra'
+      ]
+    },
+    {
+      title: 'Other',
+      tech: [
+        'Git/Github', 'Mocha', 'Chai', 'Jest', 'Enzyme', 'AWS (EC2/S3)', 'Heroku'
+      ]
+    }
+  ]
 };
+
+export const CONTACT = {
+  links: [
+    {
+      name: 'Github',
+      link: 'https://github.com/j-gens',
+      img: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/github-logo.png'
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/juliatgens/',
+      img: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/linkedin-logo.png'
+    },
+    {
+      name: 'Email',
+      link: 'mailto:juliatgens@gmail.com',
+      img: 'https://j-gens-portfolio.s3-us-west-1.amazonaws.com/email-logo.png'
+    }
+  ]
+}
