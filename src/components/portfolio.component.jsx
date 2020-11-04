3bdc4be38313ff3c694416e87e1c0b7d1376b177
j-gens <jgens2@gmail.com>
2020-11-04 15:52:04 -0800

    Add "show more/less" button to portfolio section

--- src/components/portfolio.component.jsx
+++ src/components/portfolio.component.jsx
@@ -4,20 +4,48 @@ import PortfolioApp from './portfolio-app.component.jsx';
 import { PORTFOLIO } from '../content/content.js';
 
 
-const Portfolio = () => (
-  <div className='portfolio'>
-    <span className='section-title'>
-      Portfolio
-    </span>
-    <div className='portfolio--content'>
-      {
-        PORTFOLIO.apps.map(app =>
-          <PortfolioApp key={app.name} app={app} />
-        )
-      }
-    </div>
-  </div>
-);
+class Portfolio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seeMore: false,
+      moreText: 'Show More',
+      lessText: 'Show Less',
+    };
+    this.toggleView = this.toggleView.bind(this);
+  }
+
+  toggleView() {
+    this.setState({
+      seeMore: !this.state.seeMore,
+    });
+  }
+
+  render() {
+    const { seeMore, moreText, lessText } = this.state;
+    return (
+      <div className='portfolio'>
+        <span className='section-title'>
+          Portfolio
+        </span>
+        <div className='portfolio--content'>
+          {
+            PORTFOLIO.apps
+            .filter((ele, i) => i < (seeMore ? PORTFOLIO.apps.length : 4))
+            .map(app =>
+              <PortfolioApp key={app.name} app={app} />
+            )
+          }
+        </div>
+        <div className='portfolio--button' onClick={this.toggleView}>
+          {
+            seeMore ? lessText : moreText
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default Portfolio;
