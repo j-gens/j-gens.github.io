7ac71c2eb6b6d776288a1ebb9f5f50055fe767ef
j-gens <jgens2@gmail.com>
2020-11-04 11:56:07 -0800

    Update file system layout to match single styles sheet

--- src/app.component.jsx
+++ src/app.component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import Welcome from './components/welcome/welcome.component.jsx';
-import Portfolio from './components/portfolio/portfolio.component.jsx';
-import Skills from './components/skills/skills.component.jsx';
-import AboutMe from './components/about-me/about-me.component.jsx';
-import Contact from './components/contact/contact.component.jsx';
-
-import './app.styles.css';
+import Welcome from './components/welcome.component.jsx';
+import Portfolio from './components/portfolio.component.jsx';
+import Skills from './components/skills.component.jsx';
+import AboutMe from './components/about-me.component.jsx';
+import Contact from './components/contact.component.jsx';
 
 
 const App = () => (
